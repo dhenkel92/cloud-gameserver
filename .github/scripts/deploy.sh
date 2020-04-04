27cbf360b98dae382405ab1457d40494aeec39f9
Daniel Henkel <daniel@henkel.tech>
2020-04-04 14:38:03 +0200

    fix deploy script permissions

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -3,5 +3,6 @@
 set -ex
 
 echo "$DEPLOY_SSH_PRIV_KEY" > ssh_key
-ssh -i ./ssh_key -p 2332 github-deploy@mc.henkel.tech bash -c 'deploy'
+chmod 400 ./ssh_key
+ssh -i ./ssh_key -p 2332 -o StrictHostKeyChecking=no github-deploy@mc.henkel.tech bash -c 'deploy'
 rm -f ssh_key
