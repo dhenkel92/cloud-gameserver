4490eb7c0989e7bf57a618923cc6e5ddefc84bf2
Daniel Henkel <daniel@henkel.tech>
2020-04-09 07:13:06 +0200

    update login colors

--- react-frontend/src/components/login/login.tsx
+++ react-frontend/src/components/login/login.tsx
@@ -5,18 +5,19 @@ import {faUnlockAlt} from '@fortawesome/free-solid-svg-icons';
 import {Input} from "../general/input/Input";
 import {Button} from "../general/button/Button";
 import {Link} from "react-router-dom";
+import colors from '../general/colors/Colors.module.css';
+import buttonStyle from '../general/button/Button.module.css';
 
 export class Login extends React.Component<{}, {}> {
     render() {
         return (
-            <div className="login">
-                <FontAwesomeIcon icon={faUnlockAlt} size="2x" color="#90caf9"/>
+            <div className={`login ${colors.background}`}>
+                <FontAwesomeIcon icon={faUnlockAlt} size="2x" className={colors.primaryColor}/>
                 <h2>Login</h2>
                 <div className="form">
                     <Input placeholder="Username"/>
                     <Input placeholder="Password" type="password"/>
-                    <Button name="Login" style={{marginTop: '40px'}}/>
-                    <Link to="/home">laskdfj</Link>
+                    <Link to="/home" className={`loginButton ${buttonStyle.myButton} ${colors.primary}`}>Login</Link>
                 </div>
             </div>
         );
