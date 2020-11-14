72b6a1e00e53dfce7463972f5a823a2746dcf4b5
Daniel Henkel <daniel@henkel.tech>
2020-11-14 15:51:20 +0100

    create base image with packer and install ansible

---
+++ infrastructure/packer/provisioners/common/install-ansible.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+set -ex
+
+# Install ansible
+echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" > /etc/apt/sources.list.d/ansible.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+apt-get update
+apt-get install -y ansible
+
+# Move ansible files from temp to proper folder
+mv /tmp/ansible /root
+
+cp /root/ansible/host/ansible.cfg /etc/ansible/ansible.cfg
