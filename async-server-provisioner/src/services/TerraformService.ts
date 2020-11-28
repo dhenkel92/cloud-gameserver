9bca26fef55cd2ac49885a984a4b47462b1aa117
Daniel Henkel <daniel@henkel.tech>
2020-11-28 12:07:46 +0100

    [ASP] replace terragrunt init with terraform inti

--- async-server-provisioner/src/services/TerraformService.ts
+++ async-server-provisioner/src/services/TerraformService.ts
@@ -28,7 +28,7 @@ export class TerraformService {
   }
 
   private async init(gameDeployId: number): Promise<void> {
-    const res = await this.shellAdapter.exec('cd ./terraform/02-game-server && terragrunt init');
+    const res = await this.shellAdapter.exec('cd ./terraform/02-game-server && terraform init');
     await this.writeShellLog(gameDeployId, res);
   }
 
