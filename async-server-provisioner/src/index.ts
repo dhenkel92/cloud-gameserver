1a428dcd5cee1948afb79a90bfb27097abf89731
Daniel Henkel <daniel@henkel.tech>
2021-01-25 20:59:07 +0100

    [ASP] remove tslint

--- async-server-provisioner/src/index.ts
+++ async-server-provisioner/src/index.ts
@@ -7,7 +7,6 @@ const serviceLocator = ServiceLocator.getInstance();
     const service = await serviceLocator.getGameDeploymentService();
     await service.start();
   } catch (e) {
-    // tslint:disable-next-line:no-console
     console.error(e);
   }
 })();
