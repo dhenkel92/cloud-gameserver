1276490110ee1bd813574e1cecf6df8dd93adad8
Daniel Henkel <Daniel.henkel@westwing.de>
2021-06-10 09:36:48 +0200

    [RFE] add real information to game config details page

--- react-frontend/src/components/home/GameConfigDetails/DetailsTable/DetailsTable.tsx
+++ react-frontend/src/components/home/GameConfigDetails/DetailsTable/DetailsTable.tsx
@@ -4,18 +4,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
 import { GameConfigButtons } from '../GameConfigButtons/GameConfigButtons';
 
-export class DetailsTable extends React.Component {
+interface DetailsTableProps {
+  gameName: string;
+  gameConfigName: string;
+}
+
+export class DetailsTable extends React.Component<DetailsTableProps> {
   render(): JSX.Element {
     return (
       <div className="detailsTable">
         <div className="detailsTableRow">
           <div className="detailsTableColumnLeft">Type:</div>
-          <div className="detailsTableColumnRight">Minecraft</div>
+          <div className="detailsTableColumnRight">{this.props.gameName}</div>
         </div>
         <hr />
         <div className="detailsTableRow">
           <div className="detailsTableColumnLeft">Name:</div>
-          <div className="detailsTableColumnRight">The pack - v1.0.0</div>
+          <div className="detailsTableColumnRight">{this.props.gameConfigName}</div>
         </div>
         <hr />
         <div className="detailsTableRow">
--- react-frontend/src/components/home/GameConfigDetails/GameConfigDetails.tsx
+++ react-frontend/src/components/home/GameConfigDetails/GameConfigDetails.tsx
@@ -2,38 +2,67 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import './GameConfigDetails.css';
-import { Link, RouteComponentProps } from 'react-router-dom';
+import { Link, Redirect, RouteComponentProps } from 'react-router-dom';
 import colors from '../../general/colors/Colors.module.css';
 import { DetailsTable } from './DetailsTable/DetailsTable';
 import { DetailsConsole } from './DetailsConsole/DetailsConsole';
+import { gql, useQuery } from '@apollo/client';
+
+const GAME_CONFIG_DETAILS = gql`
+  query gameConfig($id: ID!) {
+    gameConfig(id: $id) {
+      name
+      game {
+        name
+      }
+    }
+  }
+`;
+
+type GameConfigDetailsResponse = {
+  gameConfig: {
+    name: string;
+    game: {
+      name: string;
+    };
+  };
+};
 
 type GameConfigDetailsProps = {
   name: string;
-} & RouteComponentProps;
+} & RouteComponentProps<{ id?: string | undefined }>;
 
-export class GameConfigDetails extends React.Component<GameConfigDetailsProps> {
-  render(): JSX.Element {
-    return (
-      <div className="configDetailsWrapper">
-        <div className={colors.primaryColor}>
-          <Link to="/">
-            <FontAwesomeIcon icon={faArrowLeft} size="2x" />
-          </Link>
-        </div>
-        <div className="configDetailsContentWrapper">
-          <div className={`configDetails`}>
-            <div className={`test1 ${colors.surface01}`}>
-              <img alt="Minecraft" src={'https://i.computer-bild.de/imgs/1/1/5/2/9/5/0/5/Minecraft-1024x576-8b2043ae37807fa0.jpg'} />
-              <DetailsTable />
-            </div>
+export const GameConfigDetails = (props: GameConfigDetailsProps): JSX.Element => {
+  const gameConfigId = props.match.params.id;
+  const { loading, error, data } = useQuery<GameConfigDetailsResponse>(GAME_CONFIG_DETAILS, { variables: { id: gameConfigId } });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  if (!data) return <p>Error :(</p>;
+
+  return (
+    <div className="configDetailsWrapper">
+      <div className={colors.primaryColor}>
+        <Link to="/">
+          <FontAwesomeIcon icon={faArrowLeft} size="2x" />
+        </Link>
+      </div>
+      <div className="configDetailsContentWrapper">
+        <div className={`configDetails`}>
+          <div className={`test1 ${colors.surface01}`}>
+            <img
+              alt={data.gameConfig.game.name}
+              src={'https://i.computer-bild.de/imgs/1/1/5/2/9/5/0/5/Minecraft-1024x576-8b2043ae37807fa0.jpg'}
+            />
+            <DetailsTable gameName={data.gameConfig.game.name} gameConfigName={data.gameConfig.name} />
           </div>
-          <div className={`configDetailsLog`}>
-            <div className={`test2 ${colors.surface01}`}>
-              <DetailsConsole />
-            </div>
+        </div>
+        <div className={`configDetailsLog`}>
+          <div className={`test2 ${colors.surface01}`}>
+            <DetailsConsole />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
