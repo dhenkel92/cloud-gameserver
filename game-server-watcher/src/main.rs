819a67722c4d5e5927232e936d75f607961c9bef
Daniel Henkel <9447057+dhenkel92@users.noreply.github.com>
2022-04-19 13:18:51 +0200

    Fix game server watcher (#31)
    * fix game server watcher for newer rust version
    
    * add cargo.lock file
    
    Co-authored-by: Daniel Henkel <Daniel.henkel@westwing.de>

--- game-server-watcher/src/main.rs
+++ game-server-watcher/src/main.rs
@@ -9,16 +9,35 @@ extern crate rocket;
 
 use crate::routes::{watcher_details::watcher_details, watchers::watchers};
 use crate::watcher_config::{load_watcher_config, WatcherConfig};
-use clap::{load_yaml, App, ArgMatches};
+use clap::{Parser, ArgEnum};
 use rocket::figment::Figment;
 use rocket::{Build, Rocket};
 use std::error::Error;
 
-fn rocket(
-    figment: &Figment,
-    watcher_config: &WatcherConfig,
-    clap_config: &ArgMatches,
-) -> Rocket<Build> {
+#[derive(Parser, Debug, Clone)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    #[clap(long, default_value_t = String::from("127.0.0.1"), help = "IP address to serve on")]
+    address: String,
+    #[clap(long, default_value_t = 8080, help = "Port on which the Webserver will listen")]
+    port: i32,
+    #[clap(name = "base-url", long, default_value_t = String::from("http://127.0.0.1:8080"), help = "The base URL used to construct json links")]
+    base_url: String,
+    #[clap(name = "config-path", long, help = "Path to the configuration file.")]
+    config_path: String,
+    #[clap(long, arg_enum, default_value_t = ArgsVerbosity::INFO, help = "Log level verbosity.")]
+    verbosity: ArgsVerbosity,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum, Debug)]
+pub enum ArgsVerbosity {
+  DEUBG,
+  INFO,
+  WARN,
+  ERROR,
+}
+
+fn rocket(figment: &Figment, watcher_config: &WatcherConfig, clap_config: &Args) -> Rocket<Build> {
     rocket::custom(figment)
         .manage(watcher_config.clone())
         .manage(clap_config.clone())
@@ -27,30 +46,18 @@ fn rocket(
 
 #[rocket::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let clap_config = load_yaml!("clap-config.yml");
-    let matches = App::from(clap_config)
-        .version(clap::crate_version!())
-        .author(clap::crate_authors!())
-        .about(&*("\n".to_owned() + clap::crate_description!()))
-        .get_matches();
+    let args: Args = Args::parse();
 
-    println!("matches: {:?}", matches);
-
-    let port = matches.value_of_t::<u32>("port");
-    if let Err(e) = port {
-        panic!("{:?}", e);
-    }
-    let port = port.unwrap();
+    println!("matches: {:?}", args);
 
     let figment = rocket::Config::figment()
-        .merge(("address", matches.value_of("address").unwrap()))
-        .merge(("port", port));
+        .merge(("address", args.address.clone()))
+        .merge(("port", args.port));
 
-    let config_path = matches.value_of("config-path").unwrap();
-    let watcher_config = load_watcher_config(config_path)?;
+    let watcher_config = load_watcher_config(&args.config_path)?;
     println!("watcher config: {:?}", watcher_config);
 
-    if let Err(e) = rocket(&figment, &watcher_config, &matches).launch().await {
+    if let Err(e) = rocket(&figment, &watcher_config, &args).launch().await {
         println!("Rocket error: {:?}", e);
         panic!("Whoops rocket didn't launch")
     }
--- game-server-watcher/src/routes/watchers.rs
+++ game-server-watcher/src/routes/watchers.rs
@@ -1,6 +1,5 @@
 use crate::routes::types::{create_watcher_links, JsonResponseLinks};
 use crate::watcher_config::WatcherConfig;
-use clap::ArgMatches;
 use rocket::serde::{json::Json, Serialize};
 use rocket::State;
 use std::collections::HashMap;
@@ -20,9 +19,8 @@ struct AvailableWatcherData {
 #[get("/watchers")]
 pub fn watchers(
     config: &State<WatcherConfig>,
-    clap_config: &State<ArgMatches>,
+    clap_config: &State<crate::Args>,
 ) -> Json<Vec<AvailableWatcher>> {
-    let base_url = clap_config.value_of("base-url").unwrap();
     println!("{:?}", config);
     let avail = config
         .watchers
@@ -30,7 +28,7 @@ pub fn watchers(
         .map(|watch| AvailableWatcher {
             data: AvailableWatcherData {
                 name: watch.name.clone(),
-                links: create_watcher_links(base_url, &watch.name),
+                links: create_watcher_links(&clap_config.base_url, &watch.name),
             },
             links: HashMap::new(),
         })
