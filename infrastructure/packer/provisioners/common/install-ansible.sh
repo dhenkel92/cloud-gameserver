aaeb4f21b85a28f1707416fc106221402b4b2194
Daniel Henkel <Daniel.henkel@westwing.de>
2022-04-24 12:35:58 +0200

    [INFRA] don't bundle game-server-watcher with vm image

--- infrastructure/packer/provisioners/common/install-ansible.sh
+++ infrastructure/packer/provisioners/common/install-ansible.sh
@@ -2,27 +2,20 @@
 
 set -ex
 
-# Move Game Server Watcher executable
-mv /tmp/game-server-watcher /usr/bin/game-server-watcher
-
 # Install ansible
-echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" > /etc/apt/sources.list.d/ansible.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 apt-get update
-apt-get install -y ansible
-ansible-galaxy collection install amazon.aws
+apt-get install -y python3 python3-pip
+pip3 install -I ansible==5.6.0
 
 # Move ansible files from temp to proper folder
 mv /tmp/ansible /root
 mkdir -p /root/ansible/vars
 
-cp /root/ansible/host/ansible.cfg /etc/ansible/ansible.cfg
-
-cat <<EOF > /root/ansible/vars/game-server.yaml
+cat <<EOF > /root/ansible/vars/base-server.yaml
 aws:
   aws_access_key_id: placeholder
   aws_secret_access_key: placeholder
   aws_default_region: eu-central-1
 EOF
 
-ansible-playbook /root/ansible/game-server.yml --extra-vars "@/root/ansible/vars/game-server.yaml"
+ansible-playbook /root/ansible/base-server.yml --extra-vars "@/root/ansible/vars/base-server.yaml"
