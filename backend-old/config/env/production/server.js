49f903694700f79bf222bc2ac0f0db1613417f76
Daniel Henkel <daniel@henkel.tech>
2024-05-01 12:48:23 +0200

    cleanup: remove old backend

--- backend-old/config/env/production/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = () => ({
-  proxy: true,
-  url: "https://api.cloud-game.app",
-  "admin": {
-    "url": "/",
-    "host": "amin.cloud-game.app",
-    "serverAdminPanel": false,
-  },
-});
--- backend-old/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '6974ce49-975a-4a45-be32-3e9e266ae1d3'
-};
