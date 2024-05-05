26bc96c774d7d2fb27eb46def497d9e3191f2cd3
Daniel Henkel <daniel@henkel.tech>
2024-05-05 20:17:11 +0200

    cleanup: old unused ansible roles

--- infrastructure/ansible/roles/ark-server/files/server_start.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-${DIR}/ShooterGameServer TheIsland?listen?SessionName=DickerLiebhaber69?ServerPassword=reudenkobold?ServerAdminPassword=M75m7VJx2xScx9 -server -log
--- infrastructure/ansible/roles/ark-server/files/start-ark-server.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-su steam -c "/home/steam/servers/ark/ShooterGame/Binaries/Linux/server_start.sh"
