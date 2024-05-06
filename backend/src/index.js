38072d33b313c180e74db8ebc11ee968c99c0ea7
Daniel Henkel <daniel@henkel.tech>
2024-05-06 19:29:53 +0200

    feature: create new game-server-watcher

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,32 @@ module.exports = {
    * This gives you an opportunity to extend code.
    */
   register(/*{ strapi }*/) {
+    const extension = ({ nexus }) => ({
+      typeDefs: `
+        type GameServerPort {
+          port: Int!
+          protocol: String!
+          is_open: Boolean!
+        }
+        extend type GameDeployment {
+          game_server_ports: [GameServerPort!]
+        }
+      `,
+      resolvers: {
+        GameDeployment: {
+          game_server_ports: {
+            resolve: async (parent, args, context) => {
+              console.log(parent.id);
+              const data = await fetch("http://localhost:8080/ports");
+              return data.json();
+            },
+          },
+        },
+      },
+      // plugins: [nexus.plugin({})],
+    });
+
+    strapi.plugin("graphql").service("extension").use(extension);
     // const extensionService = strapi.plugin('graphql').service('extension');
     // extensionService.use({
     // resolversConfig: {
